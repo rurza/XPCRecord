1a464e6a8b585a1a7ef3f39ea71f5144f8db90ed
Adam Różyński <adam@micropixels.pl>
2024-08-14 08:44:19 +0200

    Initial Commit

---
+++ XPCRecord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XPCRecord
+//
+//  Created by Adam Różyński on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XPCRecord/XPCRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPCRecordApp.swift
+//  XPCRecord
+//
+//  Created by Adam Różyński on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct XPCRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
